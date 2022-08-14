38009119ebd504204421e30f7fd17510a8bbb847
Blandine DUCLOS <blandine.duclos.96@gmail.com>
2022-08-14 16:57:32 +0200

    modify p7

--- Projet 7 - Implémentez un modèle de scoring/API/Duclos_Blandine_2_api_model_052022.py
+++ Projet 7 - Implémentez un modèle de scoring/API/Duclos_Blandine_2_api_model_052022.py
@@ -2,14 +2,16 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pandas as pd
+import numpy as np
+from os.path import join
 import pickle
 import dill
-import pandas as pd
-from os.path import join, dirname, realpath
+import gzip
+import bz2
 
 app = FastAPI(title = "Prediction Model API for Loan"
               , description = "A simple API that use machine learning model to predict the probability of difficulties paiements loan."
-              # Can you predict how capable each applicant is of repaying a loan?
               , version = "0.1")
 
 class Loan(BaseModel):
@@ -23,7 +25,7 @@ def root():
 def features_importances_globals():
     # load training model
     try: 
-        model = pickle.load(open(join("dataviz","classifier.pkl"), "rb")) #abspath
+        model = pickle.load(bz2.open(join("dataviz","classifier.pkl"), "rb")) #abspath
     except:
         raise 'You must train the model first.'
     
@@ -32,73 +34,95 @@ def features_importances_globals():
         feat_imp = model.steps[-1][1].feature_importances_
     except:
         feat_imp = model.steps[-1][1].coef_[0]
+    feat_names = model.feature_names_in_
     del model
 
     df_feat_imp = pd.DataFrame(feat_imp
                                 , columns = ["Coefficients"]
-                                , index = model.feature_names_in_
+                                , index = feat_names
                                 )
+    del feat_imp, feat_names
+
     df_feat_imp = df_feat_imp.sort_values('Coefficients', ascending = False).head(20)
+
     return {"feat_imp": df_feat_imp}
 
 @app.post('/predict/{id_loan}')
-def predict_solvent(loan: Loan):
-    if(not(loan)):
-        raise "Please Provide a valid loan id"
-    
+async def predict_solvent(loan: Loan):
     # chargement des données explicatives
-    loanID = dict(loan)['LoanID']    
-    try:
-        df = pd.read_csv(join("dataviz",'data_test.csv'))
+    try: 
+        with bz2.open(join("dataviz",'data_test.csv')) as f:
+            row_count = sum(1 for row in f) - 1
     except:
         raise 'No application train data is available, please check the csv file.'
-    data = df[df['SK_ID_CURR'] == loanID]
-    data = data.iloc[:,1:]
-    if (data.shape[0] == 0):
+    listSizeRow = np.arange(0,row_count,10000)
+    del row_count
+    idx = -1
+    for i in np.arange(0,len(listSizeRow)):
+        df = pd.read_csv(join("dataviz",'data_test.csv'), compression = "bz2", skiprows = listSizeRow[i], nrows = 10000)
+        if loan.LoanID in df['SK_ID_CURR'].unique().tolist():
+            dataId = df[df['SK_ID_CURR'] == loan.LoanID]
+            dataId = dataId.iloc[:,1:]
+            idx = int(df[df['SK_ID_CURR'] == loan.LoanID].index.values)
+            break
+    if idx == -1:
         raise "This loan id isn't in the database"
-    del df
-    
+    del loan.LoanID, listSizeRow
+
     # chargement du model
     try: 
-        model = pickle.load(open(join("dataviz","classifier.pkl"), "rb")) #abspath
+        model = pickle.load(bz2.open(join("dataviz","classifier.pkl"), "rb")) #abspath
     except:
         raise 'You must train the model first.'
 
-    # préparation des objets pour explainer
+    # préparation des objets pour l'explainer
+    data_trans = model.steps[0][1].transform(df)
+    data_trans = data_trans[idx].tolist()
     model_proba = model.steps[-1][1].predict_proba
-    data_trans = model.steps[0][1].transform(data)
+    del df, idx
 
     # calcul des proba de prédiction
-    log_proba = model.predict_proba(data).tolist()
+    log_proba = model.predict_proba(dataId).tolist()
     del model
     if log_proba[0][1] >= .3:
         predict = 0
-        message_predict = "The data predict none of difficulties paiements. The loan is granted."
     else:
         predict = 1
-        message_predict = "The data predict difficulties paiements. The loan is refused."
-                         
-    # Explainer
-    n_feat = len(data_trans)
+        
     try: 
-        explainer = dill.load(open(join("dataviz","explainer"), "rb"))
+        with gzip.open(join("dataviz","explainer"), "rb") as f:
+            explainer = dill.load(f)
     except:
         raise 'You must download the interpretability object.'
-    exp = explainer.explain_instance(data_trans
+        
+    exp = explainer.explain_instance(np.array(data_trans)
                                     , model_proba
-                                    , num_features = n_feat
+                                    , num_features = 20
                                     )
-    del explainer
+    res_exp = exp.as_list()
+                                    
+    del explainer, data_trans, model_proba, exp
 
     return {"log_proba":log_proba
             , "prediction":predict
-            , "message_predict":message_predict
-            , "explainer":exp.as_html()
+            , "explainer":res_exp
             }
-            
+'''
+@app.post('/explainer/')
+async def get_explainer(data:list):
+    # chargement du model
+    try: 
+        model = pickle.load(bz2.open(join("dataviz","classifier.pkl"), "rb")) #abspath
+    except:
+        raise 'You must train the model first.'
+    model_proba = model.steps[-1][1].predict_proba
+    del model
+
+    return{'exp':exp.as_list()}
+'''            
 if __name__ == '__main__':
     uvicorn.run(app
-                , host = '0.0.0.0'
-                #, port = 80
-                #, debug = True # ligne à commenter lors du déploiement
+                , host = "127.0.0.1" #'0.0.0.0'
+                , port = 8000
+                , debug = True # ligne à commenter lors du déploiement
                )
--- Projet 7 - Implémentez un modèle de scoring/dashboard/Duclos_Blandine_2_dashboard_052022.py
+++ Projet 7 - Implémentez un modèle de scoring/dashboard/Duclos_Blandine_2_dashboard_052022.py
@@ -3,140 +3,212 @@ import pandas as pd
 from os.path import join, dirname, realpath
 import matplotlib.pyplot as plt
 import requests
+import pickle
+import bz2
+import gzip
+import dill
+import numpy as np
 
-try:
-    df = pd.read_csv(join("data",'data_test.csv'))
-except:
-    raise 'No application train data is available, please check the csv file.'
+@st.cache(allow_output_mutation=True)
+def load_data():
+    with st.spinner('Calculating...'):
+        # récupération de toutes les données clients
+        try:
+            df = pd.read_csv(join("data",'data_test.csv'), compression = 'bz2')
+        except:
+            raise 'No application data is available, please check the csv file.'
+        idlist = ['< No id selecte >'] + df['SK_ID_CURR'].sort_values().to_list()
 
-st.sidebar.write("""
-        ## Filters
-        Select the filter that you want and wait
-        """)
+        # récupération des noms des colonnes suivant leur type
+        catCol = []
+        numCol = []
+        for col in list(df.columns):
+            if col != 'SK_ID_CURR':
+                if df[col].nunique() <= 2:
+                    catCol.append(col)
+                else:
+                    numCol.append(col)
+    
+    return df, idlist, catCol, numCol
 
-# Selected ID Customer
-list_custId = ['< No id selecte >']
-for id in df['SK_ID_CURR'].sort_values().to_list():
-    list_custId.append(id)
-id_to_filter = st.sidebar.selectbox('Select the customer id :'
-                    , list_custId
-                    )
+@st.cache(allow_output_mutation=True)
+def print_feat_imp():
+    # graph of features importances model
+    res = requests.get(f"https://blandine-duclos-api-model.herokuapp.com/features_importances")
+    results = res.json()
+    df_feat_imp = pd.DataFrame(results.get("feat_imp")).sort_values('Coefficients', ascending = False)
+    fig, ax = plt.subplots()
+    ax.barh(df_feat_imp.index
+            , df_feat_imp.Coefficients
+        )
+    return fig
 
-catCol = []
-numCol = []
-for col in df.columns.to_list():
-    if col != 'SK_ID_CURR':
-        if df[col].nunique() <= 2:
-            catCol.append(col)
-        else:
-            numCol.append(col)
+@st.cache(allow_output_mutation=True)
+def print_predict(df, id_to_filter):
+    # récupération des données du clients
+    filtered_data = df[df['SK_ID_CURR'] == int(id_to_filter)]
+    filtered_data = filtered_data.iloc[:,1:]
+
+    # prédiction de la décision de l'accord ou non du prêt
+    res = requests.post(f"https://blandine-duclos-api-model.herokuapp.com/predict/{id_to_filter}"
+                        , json = {"LoanID": int(id_to_filter)}
+                        )
+    results = res.json()
+    log_proba = results.get("log_proba")
+    prediction = results.get("prediction")
+    list_exp = results.get("explainer")
+    if prediction == 0:
+        message_predict = "loan granted"
+    else:
+        message_predict = "loan refused"
+    return filtered_data, log_proba[0][1], prediction, message_predict, list_exp
+
+@st.cache(allow_output_mutation=True)
+def listVarUnique(catCol, numCol):
+    listColUnique = ['Choose data to display'] + numCol
+    for col in catCol:
+        idx_last_col = col.rfind('_')
+        name_col = col[:idx_last_col]
+        if name_col not in listColUnique:
+            listColUnique.append(name_col)
+
+    return listColUnique
+
+@st.cache(allow_output_mutation=True)
+def list_dataCat(df, id_to_filter, varcat):
+    dataCust = df[df['SK_ID_CURR'] != id_to_filter]
+
+    listVar = [x for x in df.columns.tolist() if x.startswith(varcat)]
+    dataCust = dataCust.loc[:,listVar]
+    valueCust = dataCust.idxmax(axis = 1).values.tolist()[0]
+
+    dfCatCol = pd.DataFrame(columns = ['Label','Value'])
+    for col in listVar:
+        idx_last_col = col.rfind('_')
+        name_value = col[idx_last_col+1:]
+        if col == valueCust:
+            valueCust = name_value
+        values = df.loc[df['SK_ID_CURR'] != id_to_filter, col].sum()
+        dict_insert = {'Label':name_value
+                        , 'Value':values}
+        dfCatCol = pd.concat([dfCatCol,pd.DataFrame([dict_insert])], ignore_index = False).reset_index(drop = True)
+
+    return valueCust, dfCatCol
+
+@st.cache(allow_output_mutation=True)
+def print_catcol(df, id_to_filter, varcat):
+    valueCust, dfCatCol = list_dataCat(df, id_to_filter, varcat)
+
+    labelList = dfCatCol['Label']
+    valueList = dfCatCol['Value']
+    fig, ax = plt.subplots()
+    ax.bar(labelList
+            , valueList
+            )
+    xi = valueCust
+    yi = dfCatCol.loc[dfCatCol['Label'] == xi,'Value'] / 2
+    ax.scatter(xi, yi)
+    
+    return fig
+
+@st.cache(allow_output_mutation=True)
+def print_numcol(dfCatCol, id_to_filter, varNum):
+    data = df.loc[df['SK_ID_CURR'] != id_to_filter, varNum]
+    fig, ax = plt.subplots()
+    ax.boxplot(data, showfliers = False)
+    ax.scatter(varNum
+                , filtered_data[varNum]
+                , c = 'red')
+    return fig
+
+@st.cache(allow_output_mutation=True)
+def print_shap(list_exp):
+    '''
+    res = requests.post(f"https://blandine-duclos-api-model.herokuapp.com/explainer/"
+                        , json = {"data": data_exp}
+                        )
+    results = res.json()
+    '''
+    plot = pd.DataFrame(list_exp, columns = ['Label','Value'])
+    color = ['r' if y < 0 else 'g' for y in plot['Value']]
+    fig, ax = plt.subplots()
+    ax.barh(plot['Label']
+            , plot['Value']
+            , color = color
+        )
+    
+    return fig
 
 st.title('My Prediction App 🎯')
 st.write("""
         This is an application to know if a loan are granted to a customer and what influenced this decision.
         """)
 
-# Selected Features importances globals
+st.sidebar.write("""
+        ## Filters
+        """)
+df, idlist, catCol, numCol = load_data()
+
+# Selected ID Customer
+id_to_filter = st.sidebar.selectbox('Select the customer id :'
+                                    , idlist
+                                    )
+
 selected_global_feat_imp = st.sidebar.checkbox('Global importance of data in the decision loan'
                                                 , value = True)
+
+# Affichage de l'importance globale des données clients dans les prédictions de l'accord ou non du prêt
 if selected_global_feat_imp:
     st.subheader(f"Global importance of data in the decision loan")
-    # graph of features importances model
-    res = requests.get(f"https://blandine-duclos-api-model.herokuapp.com/features_importances")
-    results = res.json()
-    df_feat_imp = pd.DataFrame(results.get("feat_imp"))
-    fig, ax = plt.subplots()
-    ax.barh(df_feat_imp.index
-            , df_feat_imp.Coefficients
-        )
-    st.pyplot(fig)
+    with st.spinner('Calculating...'):
+        fig = print_feat_imp()
+        st.pyplot(fig)
 
 if id_to_filter != '< No id selecte >':
-    filtered_data = df[df['SK_ID_CURR'] == int(id_to_filter)]
-    filtered_data = filtered_data.iloc[:,1:]
-    
+    # affichage des options liés aux données clients
     selected_data = st.sidebar.checkbox("Customer's data")
-    with st.spinner('Calculating...'):
-        id_loan = {"loan": str(id_to_filter)}
-        res = requests.post(f"https://blandine-duclos-api-model.herokuapp.com/predict/{id_to_filter}"
-                            , json = {"LoanID": int(id_to_filter)}
-                            )
-        results = res.json()
-        log_proba = results.get("log_proba")
-        prediction = results.get("prediction")
-        message_predict = results.get("message_predict")
-        explainer = results.get("explainer")
-        st.subheader(f"Loan decision for the customer : {id_to_filter}")
-        st.write(f"""
-                Probability of difficulties paiements : {round(log_proba[0][1],4)}
-                """)
-        st.write(f"""
-                Status Loan : {prediction}\n
-                {message_predict}
-                """)
-
-    if selected_data: # Affichage des données descriptives du customer
-        with st.spinner('Calculating...'):
-            st.subheader(f"Customer's data for the id : {id_to_filter}")
-            st.dataframe(filtered_data)
-    
-    st.sidebar.write("""
-                    ## Graphs to display
-                    """)
-    selected_comp_cust = st.sidebar.checkbox("Comparison between the customer's data and that of others")
     selected_shape_values = st.sidebar.checkbox('Importance of data in the decision loan')
-    
-    if selected_comp_cust: # Affichage des graphiques de comparaison Customer VS CustomerS
+    with st.spinner('Calculating...'):
+        listVar = listVarUnique(catCol, numCol)
+        selected_var = st.sidebar.selectbox('Select the data to display :'
+                                            , listVar
+                                            )
+
+    with st.spinner('Calculating...'):
+        filtered_data, log_proba, prediction, message_predict, list_exp = print_predict(df, id_to_filter)
+
+    st.subheader(f"Loan decision for the customer : {id_to_filter}")
+    st.write(f"""
+            Probability of difficulties paiements : {round(log_proba,4)}
+            """)
+    st.write(f"""
+            Status Loan {prediction} : {message_predict}
+            """)
+
+    # Affichage des données descriptives du customer
+    if selected_data: 
+        st.subheader(f"Customer's data for the id : {id_to_filter}")
         with st.spinner('Calculating...'):
-            st.subheader(f"Comparison between customer's data ({id_to_filter}) and that of other")
-            dataCust = df[df['SK_ID_CURR'] != id_to_filter]
-            dataCust = df.iloc[:,1:]
+            st.dataframe(filtered_data)
 
-            dfCatCol = pd.DataFrame(columns = ['Col','Label','Value'])
-            for col in catCol:
-                idx_last_col = col.rfind('_')
-                name_col = col[:idx_last_col]
-                name_value = col[idx_last_col+1:]
-                value = df.loc[df['SK_ID_CURR'] != id_to_filter, col].sum()
-                dict_insert = {'Col':name_col
-                                , 'Label':name_value
-                                , 'Value':value}
-                dfCatCol = pd.concat([dfCatCol,pd.DataFrame([dict_insert])], ignore_index = False).reset_index(drop = True)
+    # Affichage des graphiques de comparaison Customer VS CustomerS
+    if selected_var != 'Choose data to display':
 
-            listVarCat = dfCatCol['Col'].unique().tolist()
-            listVar = numCol + listVarCat
-            selected_var = st.sidebar.selectbox('Select the data to display :'
-                                                    , listVar
-                                                    )
-            with st.spinner('Calculating...'):
-                if selected_var in listVarCat:
-                    # barplot for all catCol
-                    fig, ax = plt.subplots()
-                    labelList = dfCatCol.loc[dfCatCol['Col'] == selected_var,'Label'].unique().tolist()
-                    valueList = dfCatCol.loc[dfCatCol['Col'] == selected_var,'Value']
-                    ax.bar(labelList
-                            , valueList
-                        )
-                    i = 0
-                    j = 0
-                    while i == 0:
-                        if filtered_data[selected_var+'_'+labelList[j]].values[0] == 1:
-                            xi = labelList[j]
-                            i = 1
-                    yi = dfCatCol.loc[(dfCatCol['Col'] == selected_var)&(dfCatCol['Label'] == xi),'Value'] / 2
-                    ax.scatter(xi, yi)
-                    st.pyplot(fig)
-                else:
-                    # point plot for all other col
-                    data = df.loc[df['SK_ID_CURR'] != id_to_filter, selected_var]
-                    fig, ax = plt.subplots()
-                    ax.boxplot(data, showfliers = False)
-                    ax.scatter(1
-                                , filtered_data[selected_var]
-                                , c = 'red')
-                    st.pyplot(fig)
-
-    if selected_shape_values: # Affichage des données expliquant la décision du prêt
+        st.subheader(f"Comparison between the data of client ({id_to_filter}) and the others")
+        
         with st.spinner('Calculating...'):
-            st.subheader(f"Importance of data in the loan decision for the customer : {id_to_filter}")
-            st.components.v1.html(explainer)
+            if selected_var in numCol:
+                # point plot for all other col
+                    fig = print_numcol(df, id_to_filter, selected_var)
+            else:
+                # barplot for all catCol
+                    fig = print_catcol(df, id_to_filter, selected_var)
+            st.pyplot(fig)
+
+    # Affichage des données expliquant la décision du prêt
+    if selected_shape_values: 
+        st.subheader(f"Importance of data in the loan decision for the customer : {id_to_filter}")
+
+        with st.spinner('Calculating...'):
+            fig = print_shap(list_exp)
+            st.pyplot(fig)
